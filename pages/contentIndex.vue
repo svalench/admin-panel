8cc89faf8c6e5f5ee7ba318cad4c7d36dc2e8ada
svalench <chitsalex@gmail.com>
2021-07-26 23:14:17 +0300

    добавление телефонов в админке

--- pages/contentIndex.vue
+++ pages/contentIndex.vue
@@ -24,6 +24,19 @@
     </v-col>
       </v-row>
     <v-row>
+      <v-col>
+        <v-card-text >
+        <v-btn @click="addPhone"><v-icon>mdi-plus</v-icon></v-btn>
+        <div  v-for="(phone,k) in phones" :key="k" >
+          <v-text-field label="телефон" v-model="phone.phone_number"></v-text-field>
+          <v-btn v-show="phone.id" @click="deletePhone(k)"><v-icon>mdi-delete</v-icon></v-btn>
+           <v-btn v-show="!phones[k].id" @click="savePhone(k)"><v-icon>mdi-content-save</v-icon></v-btn>
+        </div>
+
+      </v-card-text>
+      </v-col>
+    </v-row>
+    <v-row>
       <contentPreview @updateImg="updateImg" @updateData="setData" :action="'updateData'" :title.sync="i.title_block" :text.sync="i.description" :img="i.img" />
     </v-row>
     </div>
@@ -47,13 +60,39 @@ export default {
           data:[],
           activeKey:null,
           content: '',
+          phones:[],
 
         }
     },
+  computed:{
+    watchId(){
+      return this.phones.map(x=>x.id?x.id:undefined)
+    }
+  },
   mounted() {
       this.getData();
+      this.getAllPhones();
   },
   methods: {
+        addPhone(){
+        this.phones.push({phone_number:''})
+    },
+    async savePhone(k){
+          let data = await this.$axios.post(`/users/phones/`,this.phones[k]);
+      console.log(data);
+          this.phones[k] = data.data;
+          this.phones.push({});
+          this.phones.splice(this.phones.length,1);
+    },
+    async getAllPhones(){
+          let data = await this.$axios.get(`/users/phones/?limit=9999`);
+          this.phones = data.data.results;
+    },
+    async deletePhone(k){
+          if(!confirm('Вы уверены что хотите удлаить номер?')){return}
+          let data = await this.$axios.delete(`/users/phones/${this.phones[k].id}`);
+          this.phones.splice(k,1);
+    },
     /**
      *  получение данных с сервера
      */
@@ -62,7 +101,6 @@ export default {
         try {
          data = await this.$axios.get('/admin/pages/pages/1');
       }catch(e){
-          console.error(e);
           console.log(await this.$axios.post('/admin/pages/pages/',
             {"title": "First Start",
                   "h1": "First Start",
